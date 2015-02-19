22837bc0e4ceff7ad80e78c65529b84f20fa99e7
BLAZE <shootdaj@gmail.com>
2015-02-18 23:03:23 -0500

    Will now be loading external programs and external zones from the ExternalPrograms project and the ExternalZones project respectively, instead of from dll files in config. This allows the plugins (external programs and zones) to be coded within the same workflow as the project main project itself. These will be distributed with the source code, essentially making these programs and zones part of the main program.

---
+++ CopyPluginsToWebController.bat
@@ -0,0 +1,2 @@
+copy ExternalPrograms\bin\Release\ExternalPrograms.dll WebController\Programs\ExternalPrograms.dll
+copy ExternalZones\bin\Release\ExternalZones.dll WebController\Zones\ExternalZones.dll
