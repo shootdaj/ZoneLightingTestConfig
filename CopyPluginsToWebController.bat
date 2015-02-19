c49fbfcb0d1a5b75895093092b0ea2b5add1cd13
BLAZE <shootdaj@gmail.com>
2015-02-18 23:24:16 -0500

    Added step to conditionally create folders required for plugin storage.

--- CopyPluginsToWebController.bat
+++ CopyPluginsToWebController.bat
@@ -1,2 +1,5 @@
+if not exist WebController\Programs mkdir WebController\Programs
+if not exist WebController\Zones mkdir WebController\Zones
+
 copy ExternalPrograms\bin\Release\ExternalPrograms.dll WebController\Programs\ExternalPrograms.dll
 copy ExternalZones\bin\Release\ExternalZones.dll WebController\Zones\ExternalZones.dll
